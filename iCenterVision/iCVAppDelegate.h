978b42bdec0b39e4f0bfa5e51ed62d2f60976b5c
Anatolian <sertac.anadollu@uguroptik.com>
2013-12-13 16:52:35 +0200

    Initial commit from local git

---
+++ iCenterVision/iCVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iCVAppDelegate.h
+//  iCenterVision
+//
+//  Created by Sertaç Anadollu on 13.12.2013.
+//  Copyright (c) 2013 Uğur Optik Makina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCenterVision/iCVViewController.h
@@ -0,0 +1,13 @@
+//
+//  iCVViewController.h
+//  iCenterVision
+//
+//  Created by Sertaç Anadollu on 13.12.2013.
+//  Copyright (c) 2013 Uğur Optik Makina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCVViewController : UIViewController
+
+@end
